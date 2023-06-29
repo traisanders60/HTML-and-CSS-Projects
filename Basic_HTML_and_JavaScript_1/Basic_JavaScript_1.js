91ea7765bbee11027c296afcee143cd990cfa86f
traisanders60 <136763343+traisanders60@users.noreply.github.com>
2023-06-29 18:22:45 -0400

    Basic_HTML_JavaScript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var myString = "Kiss me, I'm Irish!";
+    var result = myString.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
+alert("This is an alert box!");
